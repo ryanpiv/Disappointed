95ba6d5bfa6c4ff523ec794e1064c3cb1a42477f
Ryan <ryanpiveteau@gmail.com>
2017-01-16 12:20:21 -0500

    change from video to kill pics

---
+++ vegas/vegas_config.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$(".section-landing").vegas({
+	    slides: [
+	        { src: "media/EN/kills/m_xavius.jpg" }
+	    ],
+	    delay: 10000,
+    	animation: 'random',
+    	animationDuration: 10000
+	});
+});
