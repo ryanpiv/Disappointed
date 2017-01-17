2de7202649c43590b473ad25524345645952b91d
Ryan <ryanpiveteau@gmail.com>
2017-01-17 11:33:03 -0500

    nighthold video

--- vegas/vegas_config.js
+++ vegas/vegas_config.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
 	$(".vegas-slideshow").vegas({
 	    slides: [
-	        { src: "media/EN/kills/m_xavius.jpg" }
+	    	{ src: "media/EN/kills/m_xavius.jpg" },
+	        { src: "media/nighthold_placeholder.jpg",
+	            video: {
+	                src: [
+	                    'media/nighthold.mp4'
+	                ],
+	                loop: false,
+	                mute: true
+	            },
+	            delay: 30000 
+	        }
 	    ],
-	    delay: 10000,
+	    delay: 15000,
     	animation: 'random',
-    	animationDuration: 10000
+    	animationDuration: 15000
 	});
 });
