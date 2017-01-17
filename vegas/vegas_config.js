e74929ba010460948285c48f7d8a99d494ffcb49
Ryan <ryanpiveteau@gmail.com>
2017-01-17 12:05:31 -0500

    added 1 second to video transition

--- vegas/vegas_config.js
+++ vegas/vegas_config.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 	                loop: true,
 	                mute: true
 	            },
-	            delay: 30000 
+	            delay: 31000 
 	        }
 	    ],
 	    delay: 15000,
