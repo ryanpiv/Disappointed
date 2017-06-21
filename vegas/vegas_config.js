8a31b2ce95c0d657693a411ca8fa3d4a7ce90ef1
Ryan <ryanpiveteau@gmail.com>
2017-06-21 14:22:40 -0400

    video timer

--- vegas/vegas_config.js
+++ vegas/vegas_config.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
                         loop: true,
                         mute: true
                     },
-                    delay: 27000
+                    delay: 
                 },
                 { src: "media/ToV/kills/m_helya.jpg" },
                 { src: "media/NH/kills/m_auger.jpg" },
